6553b55b237afbdb862afbcf22515828ccac174a
Henrik Bölte <hboelte@outlook.de>
2018-10-13 10:40:06 +0200

    Add project files.

---
+++ dBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
